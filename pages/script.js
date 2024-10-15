765d27e3d12309f61a5c8651595aab37a2317cc9
ImNotYejun <yejun6210@gmail.com>
2024-10-15 20:20:03 +0900

    Fix : 뭔가 JS 파일명이 script여야하는 듯

---
+++ pages/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.style.opacity = 1;
+			}
+			else{
+				entry.target.style.opacity = 0;
+			}
+		});
+	});
+
+	// 모든 .animatedTextContainer 내부의 자식 요소들을 감시
+	const animatedContainers = document.querySelectorAll(".animatedContainer");
+	animatedContainers.forEach((container) => {
+		const animatedBoxes = container.querySelectorAll("*"); // 각 container의 자식 요소들
+		animatedBoxes.forEach((entry) => {
+			observer.observe(entry); // 각 자식 요소에 observer 적용
+		});
+	});
+});
